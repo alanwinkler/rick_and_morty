dacede317034e0e26862e7d786bfd191ae899755
Alan Winkler <alanquimeywinkler@gmail.com>
2023-03-17 18:08:44 -0300

    last commit

--- front/src/redux/actions/action.js
+++ front/src/redux/actions/action.js
@@ -1,32 +1,77 @@
-export const ADD_FAVORITE = 'ADD_FAVORITE';
-export const DELETE_FAVORITE = 'DELETE_FAVORITE';
-export const FILTER = 'FILTER';
-export const ORDER = 'ORDER';
+import axios from "axios";
+export const ADD_FAVORITE = "ADD_FAVORITE";
+export const DELETE_FAVORITE = "DELETE_FAVORITE";
+export const FILTER = "FILTER";
+export const ORDER = "ORDER";
+export const GET_FAVORITES = "GET_FAVORITES";
+export const ERROR = "ERROR";
 
 export const addFavorite = (character) => {
-    return {
+  return async function (dispatch) {
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/rickandmorty/favs",
+        character
+      );
+      return dispatch({
         type: ADD_FAVORITE,
-        payload: character
+        payload: response.data, //esto va a ser el arreglo con todos los favs que vienen desde el back
+      });
+    } catch (error) {
+      return dispatch({
+        type: ERROR,
+        payload: error,
+      });
     }
-}
+  };
+};
 
 export const deleteFavorite = (id) => {
-    return {
+  return async function (dispatch) {
+    try {
+      const response = axios.delete(
+        `http://localhost:3001/rickandmorty/favs/${id}`
+      );
+      return dispatch({
         type: DELETE_FAVORITE,
-        payload: id
+        payload: response.data,
+      });
+    } catch (error) {
+      return dispatch({
+        type: ERROR,
+        payload: error,
+      });
     }
-}
+  };
+};
 
 export const filterCards = (genre) => {
-    return {
-        type: FILTER,
-        payload: genre
-    }
-}
+  return {
+    type: FILTER,
+    payload: genre,
+  };
+};
 
 export const orderCards = (order) => {
-    return {
-        type: ORDER,
-        payload: order
+  return {
+    type: ORDER,
+    payload: order,
+  };
+};
+
+export const getFavorites = () => {
+  return async function (dispatch) {
+    try {
+      const response = await axios("http://localhost:3001/rickandmorty/favs");
+      return dispatch({
+        type: GET_FAVORITES,
+        payload: response.data,
+      });
+    } catch (error) {
+      return dispatch({
+        type: ERROR,
+        payload: error,
+      });
     }
-}
+  };
+};
--- front/src/redux/reducer/reducer.js
+++ front/src/redux/reducer/reducer.js
@@ -1,43 +1,59 @@
-import { ADD_FAVORITE, DELETE_FAVORITE, FILTER, ORDER } from "../actions/action"
+import {
+  ADD_FAVORITE,
+  DELETE_FAVORITE,
+  FILTER,
+  ORDER,
+  ERROR
+} from "../actions/action";
 const initialState = {
-    myFavorites: [],
-    allCharacters: [],
-}
+  myFavorites: [],
+  allCharacters: [],
+  errors: {},
+};
 
 const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_FAVORITE:
-            return {
-                allCharacters: [...state.allCharacters, action.payload],
-                myFavorites: [...state.allCharacters, action.payload]
-            }
-            
-        case DELETE_FAVORITE:
-            return {
-                ...state,
-                myFavorites: state.myFavorites.filter((character) => character.id !== action.payload)
-            }
+  switch (action.type) {
+    case ADD_FAVORITE:
+      return {
+        ...state,
+        allCharacters: action.payload,
+        myFavorites: action.payload,
+      };
 
-        case FILTER:
-            return {
-                ...state,
-                myFavorites: state.allCharacters.filter((character) => character.genre !== action.payload)
-            }
+    case DELETE_FAVORITE:
+      return {
+        ...state,
+        myFavorites: action.payload,
+      };
 
-        case ORDER:
-            const sortedCharacters = state.allCharacters;
-            if (action.payload === "Ascendente"){
-                sortedCharacters.sort((a,b) => a.id - b.id)
-            } else {
-                sortedCharacters.sort((a,b) => b.id - a.id)
-            }
-            return {
-                ...state,
-                myFavorites: sortedCharacters
-            }
-        default:
-            return {...state};
-    }
-}
+    case FILTER:
+      return {
+        ...state,
+        myFavorites: state.allCharacters.filter(
+          (character) => character.genre !== action.payload
+        ),
+      };
+
+    case ORDER:
+      const sortedCharacters = state.allCharacters;
+      if (action.payload === "Ascendente") {
+        sortedCharacters.sort((a, b) => a.id - b.id);
+      } else {
+        sortedCharacters.sort((a, b) => b.id - a.id);
+      }
+      return {
+        ...state,
+        myFavorites: sortedCharacters,
+      };
+
+    case ERROR:
+      return {
+        ...state,
+        errors: action.payload,
+      };
+    default:
+      return { ...state };
+  }
+};
 
 export default rootReducer;
